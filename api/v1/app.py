9358101f235674eebd97ba8237372c42bd550ba9
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-17 18:25:09 -0500

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-
+"""Module for Flask REST app"""
+from flask import Flask, jsonify, make_response
 from models import storage
-from flask import Flask
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.teardown_appcontext
-def close(error):
-    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 not found message"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
