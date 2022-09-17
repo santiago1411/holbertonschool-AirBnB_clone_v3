84e6b9cc89c5bf90e787bc437671fa8420b72eb9
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-17 18:19:24 -0500

    States

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ def Api_Status():
     """ Status of API Returned in Json"""
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats')
+def count():
+    """Returns the number of each objects by type"""
+    objects = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    objects = {k: storage.count(v) for k, v in objects.items()}
+    return jsonify(objects)
